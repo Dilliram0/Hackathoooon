8bed7f2a36fc22e401fbed6559589a915a2d4b8d
Dilliram <bradjohn235@gmail.com>
2026-07-23 15:13:18 +0545

    Add task functionality

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("App Started");
+const input = document.getElementById("taskInput");
+const button = document.querySelector("button");
+const list = document.getElementById("taskList");
+
+button.addEventListener("click", () => {
+
+    const li = document.createElement("li");
+
+    li.textContent = input.value;
+
+    list.appendChild(li);
+
+    input.value = "";
+});
